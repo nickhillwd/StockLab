b5c35875498ab1649c683f3cf61040abf22df8ce
nickhillwd <nickhillwd@gmail.com>
2015-12-01 15:12:18 +0000

    layout and controller linked and yahoofinance required

--- controller.rb
+++ controller.rb
@@ -1,4 +1,25 @@
 require 'sinatra'
 require 'sinatra/contrib/all' if development?
 require 'pry-byebug'
+require 'yahoofinance'
 
+get '/' do
+  #binding.pry
+  @stock_symbol = params[:stock_symbol]
+
+  erb :result
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+#YahooFinance::get_standard_quotes('AAPL')['AAPL'].lastTrade
