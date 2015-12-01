c42b9e529871e3d1e8b681288bc6f9a3dbad4a2e
nickhillwd <nickhillwd@gmail.com>
2015-12-01 15:35:19 +0000

    stock function running

--- controller.rb
+++ controller.rb
@@ -4,9 +4,12 @@ require 'pry-byebug'
 require 'yahoofinance'
 
 get '/' do
-  #binding.pry
   @stock_symbol = params[:stock_symbol]
 
+  if @stock_symbol
+    @result = YahooFinance::get_standard_quotes(@stock_symbol)[@stock_symbol].lastTrade
+  end
+  # binding.pry
   erb :result
 end
 
