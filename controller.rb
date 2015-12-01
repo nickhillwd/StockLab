171bf9ba207f5149cea8e60ba69737adf23b48e3
nickhillwd <nickhillwd@gmail.com>
2015-12-01 16:13:43 +0000

    share name functionality added

--- controller.rb
+++ controller.rb
@@ -5,9 +5,12 @@ require 'yahoofinance'
 
 get '/' do
   @stock_symbol = params[:stock_symbol]
+  @stock_name = params[:stock_symbol]
 
   if @stock_symbol
-    @result = YahooFinance::get_standard_quotes(@stock_symbol)[@stock_symbol].lastTrade
+    quotes = YahooFinance::get_standard_quotes(@stock_symbol)[@stock_symbol]
+    @last_trade = quotes.lastTrade
+    @stock_name = quotes.name
   end
   # binding.pry
   erb :result
@@ -26,3 +29,4 @@ end
 
 
 #YahooFinance::get_standard_quotes('AAPL')['AAPL'].lastTrade
+#YahooFinance::get_standard_quotes('AAPL')['AAPL'].name
